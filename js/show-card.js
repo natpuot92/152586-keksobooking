304ee0c41f3092db91b978dfe37dad3c04faec67
natpuot92 <natpuot1992@gmail.com>
2017-04-24 21:31:44 +0300

    show-card synchronize-fields

--- js/show-card.js
+++ js/show-card.js
@@ -2,19 +2,18 @@
 
 (function () {
   window.showCard = function showCard(activePin, cardDialog, callback) {
-      activePin.addEventListener('click', function (evt) {
+    activePin.addEventListener('click', function (evt) {
+      cardDialog.classList.remove('hidden');
+      window.renderCard(window.advertisements[activePin.getAttribute('data-set')]);
+      callback(evt);
+    });
+
+    activePin.addEventListener('keydown', function (evt) {
+      if (window.utils.isEnterPressed(evt)) {
         cardDialog.classList.remove('hidden');
         window.renderCard(window.advertisements[activePin.getAttribute('data-set')]);
         callback(evt);
-      });
-
-      activePin.addEventListener('keydown', function (evt) {
-        if (window.utils.isEnterPressed(evt)) {
-          cardDialog.classList.remove('hidden');
-          window.renderCard(window.advertisements[activePin.getAttribute('data-set')]);
-          callback(evt);
-        }
-      });
-    }
-    return showCard;
-  })();
+      }
+    });
+  };
+})();
--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,13 +1,13 @@
 'use strict';
-(function() {
-window.synchronizeFields = function (changeField, suncField, changeFieldValues, suncFieldValues, callback) {
-  changeField.addEventListener('change', function () {
-    for (var i = 0; i < changeFieldValues.length; i++) {
-      if (changeField.value === changeFieldValues[i]) {
-        callback(suncField, suncFieldValues[i]);
-        break;
+(function () {
+  window.synchronizeFields = function (changeField, suncField, changeFieldValues, suncFieldValues, callback) {
+    changeField.addEventListener('change', function () {
+      for (var i = 0; i < changeFieldValues.length; i++) {
+        if (changeField.value === changeFieldValues[i]) {
+          callback(suncField, suncFieldValues[i]);
+          break;
+        }
       }
-    }
-  });
-};
+    });
+  };
 })();
