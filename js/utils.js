32e0bc74390eda6e9b930db896bdb68685e465e1
natpuot92 <natpuot1992@gmail.com>
2017-04-26 00:08:22 +0300

    module6-task1 четвертая попытка

--- js/utils.js
+++ js/utils.js
@@ -1,14 +1,5 @@
 'use strict';
 (function () {
-  var getRandomDigit = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-  var getArrayRandomNumber = function (array) {
-    var randomNumberArray = getRandomDigit(0, array.length - 1);
-    return array[randomNumberArray];
-  };
-
   var KEY_CODE_ENTER = 13;
 
   var KEY_CODE_ESC = 27;
