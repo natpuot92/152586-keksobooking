b5c03321fa0473566a6df92a8917dbf358f9e55a
natpuot92 <natpuot1992@gmail.com>
2017-04-27 01:00:17 +0300

    module7-task1

--- js/pin.js
+++ js/pin.js
@@ -1,39 +1,6 @@
 'use strict';
 
 (function () {
-  var parentPinMap = document.querySelector('.tokyo__pin-map');
-
-  var fragment = document.createDocumentFragment();
-
-  var errorHandler = function (errorMessage) {
-    var node = document.createElement('div');
-    node.classList.add('error');
-    node.textContent = errorMessage;
-    document.body.insertAdjacentElement('afterbegin', node);
-  };
-
-  var successHandler = function (data) {
-    window.advertisements = data;
-
-    for (var j = 0; j < data.length; j++) {
-      var childDiv = document.createElement('div');
-      childDiv.className = 'pin';
-      childDiv.style.left = data[j].location.x + 'px';
-      childDiv.style.top = data[j].location.y + 'px';
-      childDiv.setAttribute('data-set', j);
-      childDiv.setAttribute('tabindex', '0');
-      childDiv.innerHTML = '<img src=" ' + data[j].author.avatar + ' " class="rounded" width="40" height="40"></div>';
-      fragment.appendChild(childDiv);
-    }
-
-    parentPinMap.appendChild(fragment);
-
-    var pins = document.querySelectorAll('.pin:not(.pin__main)');
-
-    for (var pinIndex = 0; pinIndex < pins.length; pinIndex++) {
-      window.showCard(pins[pinIndex], window.offerDialog, window.dialogOpenHandler);
-    }
-  };
 
   var PIN_ACTIVE_CLASS_NAME = 'pin--active';
   var deleteActivePin = function () {
@@ -50,6 +17,90 @@
     setActivePin: setActivePin
   };
 
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.classList.add('error');
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  var successHandler = function (data) {
+    window.advertisements = data;
+    window.renderPin(data);
+    window.onMomentFilter = window.advertisements;
+  };
+
   window.load('https://intensive-javascript-server-kjgvxfepjl.now.sh/keksobooking/data', successHandler, errorHandler);
-})();
+
+
+
+  var filterElements = document.querySelectorAll('.tokyo__filter');
+
+  var filteredAdvertisements = [];
+//  filterElements.forEach(function (filterElement) {
+//  filterElement.addEventListener('change', function () {
+
+  var typeHouse = document.querySelector('#housing_type');
+  var housingPrice = document.querySelector('housing_price');
+  var roomNumber = document.querySelector('#housing_room-number');
+  var housingGuests = document.querySelector('#housing_guests-number');
+
+  var selectOptionValues = {
+    typeHouse: "any",
+    housingPrice: "middle",
+    roomNumber: "1",
+    housingGuests: "any"
+  }
+
+  typeHouse.addEventListener('change', function () {
+
+    delete selectOptionValues.typeHouse;
+    for (var i = 0; i <  typeHouse.options.length; i++) {
+      if (typeHouse.options[i].selected) {
+        selectOptionValues.typeHouse = typeHouse.options[i].value;
+        break;
+      }
+    }
+    debugger
+      window.filteredAdvertisements = window.advertisements;
+      if ((!(selectOptionValues.typeHouse === 'any'))) {
+        window.filteredAdvertisements = window.filteredAdvertisements.filter(function (it) {
+         return it.offer.type === selectOptionValues.typeHouse;
+        })
+      };
+      if ((!(selectOptionValues.roomNumber === 'any'))) {
+      window.filteredAdvertisements =  window.filteredAdvertisements.filter(function (it) {
+        return it.offer.rooms === parseInt(selectOptionValues.roomNumber);
+      });
+      };
+
+      console.log(window.filteredAdvertisements);
+
+      window.pins.forEach(function (pin) {
+      pin.classList.add('hidden');
+    });
+
+     window.renderPin(window.filteredAdvertisements);
+  })
+  })();
+
+
+
+//var filterArrayRoomNumber = []
+//
+//
+//  roomNumber.addEventListener('change', function () {
+//    filterArrayRoomNumber = window.onMomentFilter.filter(function (it) {
+//      return it.offer.rooms === parseInt(roomNumber.value);
+//      });
+//
+//      window.pins.forEach(function (pin) {
+//      pin.classList.add('hidden');
+//    });
+//     window.renderPin(window.onMomentFilter);
+//  })
+//
+
+
+
 
---
+++ js/render-pin.js
@@ -0,0 +1,26 @@
+'use strict';
+(function () {
+  window.renderPin = function (dataPins) {
+    var parentPinMap = document.querySelector('.tokyo__pin-map');
+    var fragment = document.createDocumentFragment();
+
+    for (var j = 0; j < dataPins.length; j++) {
+      var childDiv = document.createElement('div');
+      childDiv.className = 'pin';
+      childDiv.style.left = dataPins[j].location.x + 'px';
+      childDiv.style.top = dataPins[j].location.y + 'px';
+      childDiv.setAttribute('data-set', j);
+      childDiv.setAttribute('tabindex', '0');
+      childDiv.innerHTML = '<img src=" ' + dataPins[j].author.avatar + ' " class="rounded" width="40" height="40"></div>';
+      fragment.appendChild(childDiv);
+    }
+
+    parentPinMap.appendChild(fragment);
+
+    window.pins = document.querySelectorAll('.pin:not(.pin__main)');
+
+    for (var pinIndex = 0; pinIndex < pins.length; pinIndex++) {
+      window.showCard(pins[pinIndex], window.offerDialog, window.dialogOpenHandler);
+    }
+  };
+})();
