384e9c87604dc664daa52578cf0ed45ad2c199a0
natpuot92 <natpuot1992@gmail.com>
2017-05-17 15:38:32 +0300

    fotos

--- js/fotos.js
+++ js/fotos.js
@@ -1,10 +1,9 @@
 'use strict';
 (function () {
+
 var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 var avatarFileChooser = document.querySelector('.drop-zone input[type=file]')
 var previewAvatar = document.querySelector('.notice__preview img');
-var housingFileChooser = document.querySelector('.form__photo-container input[type=file]');
-console.log(housingFileChooser)
 
 avatarFileChooser.addEventListener('change', function () {
   var file = avatarFileChooser.files[0];
@@ -24,5 +23,39 @@ avatarFileChooser.addEventListener('change', function () {
     }
 })
 
+var housingFileChooser = document.querySelector('.form__photo-container input[type=file]');
+var previewHousing = document.querySelectorAll('.form__photo');
+var fragment = document.createDocumentFragment();
+
+for (var i = 0; i < previewHousing.length; i++) {
+  var childImg = document.createElement('img');
+  childImg.className = 'img-foto img-foto-' + i;
+  childImg.setAttribute('width', '45');
+  childImg.setAttribute('height', '45');
+  previewHousing[i].appendChild(childImg);
+}
+
+  var previewHousingImg = document.querySelectorAll('.img-foto');
+
+  housingFileChooser.addEventListener('change', function () {
+    var file = housingFileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+    var matches = FILE_TYPES.some(function (format) {
+    return fileName.endsWith(format);
+  });
+
+  if (matches) {
+    var reader = new FileReader();
+    reader.addEventListener('load', function () {
+      for (var i = 0; i < previewHousingImg.length; i++) {
+        if (previewHousingImg[i].src === '') {
+          return previewHousingImg[i].src = reader.result;
+        }
+      }
+    });
+
+    reader.readAsDataURL(file);
+  }
+  })
 
 })();
